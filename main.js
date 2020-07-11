15772ec418d5dac094210f4824852553e66dd439
Julián Absatz <julianabsatz@gmail.com>
2020-07-11 23:22:11 +0200

    separado en modulos

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import allTickets from './tickets.js'
+
 function mainLoop(){
     if(!gameState.pause){
         if(inMainLoop === true){
@@ -93,63 +95,10 @@ function drawLastTicket(ticketNumber){
 
 let openPos = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
 const availableTickets = [1,2,3,5];
-const allTickets = {
-    1:{
-        'id': 1,
-        'title': 'work',
-        'clicks': 10,
-        'anxietyPerTick': 0.1,
-        'anxietyRelief': 2,
-        'happinessRelief': 1
-    },
-    2:{
-        'id': 2,
-        'title': 'study',
-        'clicks': 10,
-        'anxietyPerTick': 0.1,
-        'anxietyRelief': 2,
-        'happinessRelief': 1
-    },
-    3:{
-        'id': 3,
-        'title': 'take a walk',
-        'clicks': 5,
-        'anxietyPerTick': 0.1,
-        'anxietyRelief': 2,
-        'happinessRelief': 1
-    },
-    4:{
-        'id': 4,
-        'title': 'pet a cat',
-        'clicks': 2,
-        'anxietyPerTick': 0,
-        'anxietyRelief': 3,
-        'happinessRelief': 2
-    },
-    5:{
-        'id': 5,
-        'title': 'get a cat',
-        'clicks': 5,
-        'anxietyPerTick': 0,
-        'anxietyRelief': 1,
-        'happinessRelief': 10,
-        'unlocks': [4,6],
-        'unique': true
-    },
-    6:{
-        'id': 6,
-        'title': 'get a cat',
-        'clicks': 5,
-        'anxietyPerTick': 0,
-        'anxietyRelief': 1,
-        'happinessRelief': 10
-    }
-};
-
 
 let lastTicket = 0;
 const nowTickets = {};
-inMainLoop = false;
+var inMainLoop = false;
 const gameState = {
     anxietyPerTick : 0,
     pause: false
